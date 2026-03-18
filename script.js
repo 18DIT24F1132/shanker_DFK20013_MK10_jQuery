8fc5c875d5230aac9e98fae2c4c1a4bdf3f6c196
18DIT24F1132 <shankerpro77@gmail.com>
2026-03-18 09:44:47 +0800

    Refactor login handling with jQuery
    Removed the semak function and replaced it with a jQuery-based login handler that opens a popup for empty fields and navigates to another page on valid input.

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-function semak()
-{
-    var username = $("#username").val();
-    var password = $("#password").val();
+$(document).ready(function () {
 
-    if(username == "" || password == "")
-    {
-        alert("Please fill in username and password");
-        return false;
-    }
+    $("#loginBtn").click(function () {
 
-    if(username == "shanker" && password == "1234")
-    {
-        alert("Login Successful");
-        return true;
-    }
-    else
-    {
-        alert("Wrong username or password");
-        return false;
-    }
-}
+        var user = $("#username").val();
+        var pass = $("#password").val();
+
+        if (user == "" || pass == "") {
+            $("#popupLogin").popup("open");
+        }
+        else {
+            $.mobile.changePage("#page2");
+        }
+
+    });
+
+});
